2c80052447ae0eec236db298708cc75ede8bbaab
aishwarya2198 <50906116+aishwarya2198@users.noreply.github.com>
2026-05-22 17:39:34 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let low = 1;
+        let high = 0;
+        for (let pile of piles) {
+            high = Math.max(pile, high);
+        }
+        while (low <= high) {
+            const mid = low + Math.floor((high - low) / 2);
+            let hours = 0;
+            for (let pile of piles) {
+                hours += Math.ceil(pile / mid);
+            }
+            if (hours > h) low = mid + 1;
+            else {
+                high = mid - 1;
+            }
+        }
+        // Return the smallest speed
+        return low;
+    }
+}
